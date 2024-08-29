587efd1026069230abe02956337726711084db44
Aidin Ahmadian <ahmadian_aidin@yahoo.com>
2024-08-29 12:27:29 +0330

    Add Screenshots/GIFs

--- Drawing App/Controllers/BaseDrawController.swift
+++ Drawing App/Controllers/BaseDrawController.swift
@@ -15,7 +15,7 @@ class BaseDrawController: UIViewController, UIPopoverPresentationControllerDeleg
     let drawSomethingLabel: UILabel = {
         let label = UILabel()
         label.text = "Draw Something!"
-        label.font = UIFont.customFont(name: "Kalam-Regular", size: 20)
+        label.font = UIFont.customFont(name: "SankofaDisplay-Regular", size: 20)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         return label
